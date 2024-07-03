1bdf20b2f5e7104576dd422ae26bcde814a8385f
sam1246 <sam@apps-craft.com>
2024-07-03 13:21:40 +0100

    Drawer navigation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet, Alert,  } from "react-native";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  useWindowDimensions,
+} from "react-native";
 import PrimaryButton from "./components/ui/PrimaryButton";
 import Colors from "./constants/colors";
 import Title from "./components/ui/Title";
@@ -8,6 +14,8 @@ import InstructionText from "./components/ui/InstructionText";
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
+
+  const { width, height } = useWindowDimensions();
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -30,8 +38,10 @@ function StartGameScreen({ onPickNumber }) {
     onPickNumber(chosenNumber);
   }
 
+const marginTop = height < 300 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTop}]}>
       <Title> Guess The Number </Title>
       <Card>
         <InstructionText>Enter your Number</InstructionText>
